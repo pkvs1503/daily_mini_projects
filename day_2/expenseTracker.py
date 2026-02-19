c52a2692acbd5a84e69067af5a229576fbaf41c5
pkvs1503 <pkvs1503@gmail.com>
2026-02-19 22:54:43 +0530

    provided my concept for code and logic in the comments.
    explained my code concept and logics which I used while programming my code and provided all in the comments.

--- day_2/expenseTracker.py
+++ day_2/expenseTracker.py
@@ -1,15 +1,20 @@
 import os
+# this module lets us allow do file operations provides file handling methods.
 
 FILE = "records.txt"
+# i am using this as my file name i stored it a variable called FILE which i need for to use in file methods.
 
 def add_income():
     amount = float(input("Enter income amount: "))
+    # i am asking user to enter the amount.
     with open(FILE, "a") as f:
+        # with this above file method standard i will write the recieved input in it as follows.
         f.write(f"Income,{amount}\n")
     print("Income added successfully!\n")
 
 def add_expense():
     amount = float(input("Enter expense amount: "))
+    # same logic is applied but now asking for expense amount.
     with open(FILE, "a") as f:
         f.write(f"Expense,{amount}\n")
     print("Expense added successfully!\n")
@@ -18,6 +23,7 @@ def show_records():
     if not os.path.exists(FILE):
         print("No records found!\n")
         return
+        # for showing recors first os module or os must see file exists or not, if no file then simply return. first error check.
     with open(FILE, "r") as f:
         print("\n--- All Records ---")
         print(f.read())
@@ -26,9 +32,12 @@ def calculate_balance():
     if not os.path.exists(FILE):
         print("No records found!\n")
         return
+        # for calculating balacne the concept used is, i will take file line and used strip for that splitted by , and store it in type, and amount.
     income = 0
     expense = 0
+    # then there is two type income and expense, so i need two variables for them.
     with open(FILE, "r") as f:
+        # opening file in read mode and as f and for each line in the file ie f , the above concept logic i implemented.
         for line in f:
             type_, amount = line.strip().split(",")
             if type_ == "Income":
@@ -37,6 +46,7 @@ def calculate_balance():
                 expense += float(amount)
     print(f"\nTotal Income: {income}")
     print(f"Total Expense: {expense}")
+    # then this simple math logic.
     print(f"Current Balance: {income - expense}\n")
 
 def menu():
@@ -64,3 +74,4 @@ def menu():
             print("Invalid choice!\n")
 
 menu()
+
